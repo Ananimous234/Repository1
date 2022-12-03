d97dfdc83849e88cf7587764bdf74b32908f6f2b
Ananimous234 <119108999+Ananimous234@users.noreply.github.com>
2022-12-03 12:20:23 +0200

    Version 1.1

---
+++ main.py
@@ -0,0 +1,22 @@
+class Tree:
+    template = "Green grass"
+    def __init__(self):
+        self.height = "3.44m"
+        self.age = "156 years"
+        self.forest = "The big lake"
+class mobilephone:
+    usbsamsung = "Type-s"
+    def __init__(self):
+        self.color = "green"
+        self.model = "S22+"
+        self.id = "S032YRT"
+tr1 = Tree()
+mb1 = mobilephone()
+print("Tree:")
+print("HEIGHT:",tr1.height)
+print("AGE:",tr1.age)
+print("FOREST:",tr1.forest)
+print("Mobile:")
+print("COLOR:",mb1.color)
+print("MODEL:",mb1.model)
+print("ID:",mb1.id)
